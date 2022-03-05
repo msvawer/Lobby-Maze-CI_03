ab99f07b8260e8096adb3d09710881d23fa8b44e
MichVawer <michelle.vawer@icloud.com>
2022-03-05 15:27:17 -0500

    using main camera to trigger return to lobby

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CheckWin : MonoBehaviour
+{
+
+    public UnityEvent onWin = new UnityEvent();
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+     private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("MainCamera"))
+            {
+            Debug.Log("you win or MainCamera tag entered");
+            onWin?.Invoke();
+        }
+    }
+}
